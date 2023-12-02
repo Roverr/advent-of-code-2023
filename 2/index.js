fdd2f863c3e8ece0a42a097c9767f41eea7a2760
Imre Racz <imre@riltech.co>
2023-12-02 08:39:51 +0100

    feat(2.5): day 2 complete

--- 2/index.js
+++ 2/index.js
@@ -1,16 +1,14 @@
 const { games } = require("./data");
 
 const sum = games.reduce((possibles, current) => {
-  const isFailed = current.results.reduce((isFailed, drawResult) => {
-    return (
-      isFailed ||
-      drawResult.red > 12 ||
-      drawResult.green > 13 ||
-      drawResult.blue > 14
-    );
-  }, false);
-  if (isFailed) return possibles;
-  return [...possibles, Number(current.id)];
-}, []).reduce((sum, curr) => sum + curr, 0);
+  const localMaxes = current.results.reduce((tracker, result) => {
+    if (tracker.red < result.red) tracker.red = result.red;
+    if (tracker.green < result.green) tracker.green = result.green;
+    if (tracker.blue < result.blue) tracker.blue = result.blue;
+    return tracker;
+  }, { red: 0, green: 0, blue: 0});
+  const power = localMaxes.red * localMaxes.green * localMaxes.blue;
+  return [...possibles, power];
+}, []).reduce((sum, p) => sum + p, 0);
 
-console.log(sum)
+console.log(sum);
