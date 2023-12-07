b1b01d608e06c128316363af9e40d257e0d6fbb2
Imre Racz <imre@riltech.co>
2023-12-07 14:03:00 +0100

    feat(7.5): part two done

--- 7/index.js
+++ 7/index.js
@@ -82,14 +82,6 @@ const calculatedHands = hands
   })
   .sort((a, b) => a.strength - b.strength);
 
-const fiveOfAKinds = calculatedHands
-  .filter(({ strength }) => strength === 0)
-  .sort(
-    (a, b) =>
-      a.tracker.findIndex(({ count }) => count === 5) -
-      b.tracker.findIndex(({ count }) => count === 5)
-  );
-
 const sorter = (a, b) => {
   const aIndexes = [];
   const bIndexes = [];
@@ -103,6 +95,14 @@ const sorter = (a, b) => {
   }
   return 0;
 };
+
+const fiveOfAKinds = calculatedHands
+  .filter(({ strength }) => strength === 0)
+  .sort(
+    (a, b) =>
+      a.tracker.findIndex(({ count }) => count === 5) -
+      b.tracker.findIndex(({ count }) => count === 5)
+  );
 const fourOfAKinds = calculatedHands
   .filter(({ strength }) => strength === 1)
   .sort(sorter);
@@ -127,7 +127,7 @@ const highCards = calculatedHands
   .filter(({ strength }) => strength === 6)
   .sort(sorter);
 
-const sortedHands = [
+let sortedHands = [
   ...fiveOfAKinds,
   ...fourOfAKinds,
   ...fullHouses,
@@ -137,9 +137,196 @@ const sortedHands = [
   ...highCards,
 ];
 
-const sum = sortedHands.reduce(
-  (sum, hand, i) => sum + Number(hand.bid) * (sortedHands.length - i),
-  0
-);
+const getSum = () =>
+  sortedHands.reduce(
+    (sum, hand, i) => sum + Number(hand.bid) * (sortedHands.length - i),
+    0
+  );
 
-console.log(`Part 1: ${sum}`);
+console.log(`Part 1: ${getSum()}`);
+
+const needsReposition = sortedHands
+  .filter(({ hand, strength }) => strength !== 0 && hand.includes("J"))
+  .map((v) => {
+    let i = -1;
+    const jPosition = 3;
+    const pretendTracker = JSON.parse(JSON.stringify(v.tracker));
+    switch (v.strength) {
+      case 1:
+        if (v.tracker[jPosition].count === 4) {
+          i = v.tracker.findIndex(({ count }) => count === 1);
+        } else {
+          i = v.tracker.findIndex(({ count }) => count === 4);
+        }
+        pretendTracker[i].count = 5;
+        pretendTracker[jPosition].count = 0;
+        return {
+          ...v,
+          pretend: {
+            tracker: pretendTracker,
+            hand: `${pretendTracker[i].key.repeat(5)}`,
+            strength: 0,
+          },
+        };
+      case 2:
+        i = -1;
+        if (v.tracker[jPosition].count > 2) {
+          i = v.tracker.findIndex(({ count }) => count === 2);
+        } else {
+          i = v.tracker.findIndex(({ count }) => count === 3);
+        }
+        pretendTracker[i].count = 5;
+        pretendTracker[jPosition].count = 0;
+        return {
+          ...v,
+          pretend: {
+            tracker: pretendTracker,
+            hand: `${pretendTracker[i].key.repeat(5)}`,
+            strength: 0,
+          },
+        };
+      case 3:
+        i = -1;
+        if (v.tracker[jPosition].count === 3) {
+          i = v.tracker.findIndex(({ count }) => count === 2);
+          if (i > -1) {
+            pretendTracker[i].count = 5;
+            pretendTracker[jPosition].count = 0;
+            return {
+              ...v,
+              pretend: {
+                tracker: pretendTracker,
+                hand: `${pretendTracker[i].key.repeat(5)}`,
+                strength: 0,
+              },
+            };
+          }
+          i = v.tracker.findIndex(({ count }) => count === 1);
+          pretendTracker[i].count = 4;
+          pretendTracker[jPosition].count = 0;
+          return {
+            ...v,
+            pretend: {
+              tracker: pretendTracker,
+              hand: v.hand.replaceAll("J", pretendTracker[i].key),
+              strength: 1,
+            },
+          };
+        }
+        i = v.tracker.findIndex(({ count }) => count === 3);
+        if (v.tracker[jPosition].count === 2) {
+          pretendTracker[i].count = 5;
+          pretendTracker[jPosition].count = 0;
+          return {
+            ...v,
+            pretend: {
+              tracker: pretendTracker,
+              hand: `${v.hand.replaceAll("J", pretendTracker[i].key)}`,
+              strength: 0,
+            },
+          };
+        }
+        pretendTracker[i].count = 4;
+        pretendTracker[jPosition].count = 0;
+        return {
+          ...v,
+          pretend: {
+            tracker: pretendTracker,
+            hand: `${v.hand.replaceAll("J", pretendTracker[i].key)}`,
+            strength: 1,
+          },
+        };
+      case 4:
+        i = -1;
+        if (v.tracker[jPosition].count === 2) {
+          i = v.tracker.findIndex(
+            ({ count, key }) => count === 2 && key !== "J"
+          );
+          pretendTracker[i].count = 4;
+          pretendTracker[jPosition].count = 0;
+          return {
+            ...v,
+            pretend: {
+              tracker: pretendTracker,
+              hand: v.hand.replaceAll("J", v.tracker[i].key),
+              strength: 1,
+            },
+          };
+        }
+        i = v.tracker.findIndex(({ count }) => count === 2);
+        pretendTracker[i].count = 3;
+        pretendTracker[jPosition].count = 0;
+        return {
+          ...v,
+          pretend: {
+            tracker: pretendTracker,
+            hand: v.hand.replaceAll("J", v.tracker[i].key),
+            strength: 2,
+          },
+        };
+      case 5:
+        i = v.tracker.findIndex(({ count, key }) => count === 1 && key !== "J");
+        pretendTracker[i].count = 3;
+        pretendTracker[jPosition].count = 0;
+        return {
+          ...v,
+          pretend: {
+            tracker: pretendTracker,
+            hand: v.hand.replaceAll("J", v.tracker[i].key),
+            strength: 3,
+          },
+        };
+      case 6:
+        i = v.tracker.findIndex(({ count, key }) => count === 1 && key !== "J");
+        pretendTracker[i].count = 2;
+        pretendTracker[jPosition].count = 0;
+        return {
+          ...v,
+          pretend: {
+            tracker: pretendTracker,
+            hand: v.hand.replaceAll("J", v.tracker[i].key),
+            strength: 5,
+          },
+        };
+    }
+  });
+
+sortedHands = sortedHands
+  .map((v) => {
+    const jPosition = v.tracker.findIndex(({ key }) => key === "J");
+    if (
+      !v.hand.includes("J") ||
+      v.tracker.find(({ key, count }) => key === "J" && count === 5)
+    ) {
+      const jObj = v.tracker[jPosition];
+      v.tracker.splice(jPosition, 1);
+      v.tracker.push(jObj);
+      return v;
+    }
+    const reposition = needsReposition.find(
+      (reposition) => reposition.hand === v.hand
+    );
+    const jObj = reposition.tracker[jPosition];
+    reposition.tracker.splice(jPosition, 1);
+    reposition.tracker.push(jObj);
+    return reposition;
+  })
+  .sort((a, b) => {
+    const aStrength = a.pretend ? a.pretend.strength : a.strength;
+    const bStrength = b.pretend ? b.pretend.strength : b.strength;
+    return aStrength - bStrength;
+  })
+  .reduce(
+    (collection, v) => {
+      if (v.pretend) {
+        collection[v.pretend.strength].push(v);
+      } else {
+        collection[v.strength].push(v);
+      }
+      return collection;
+    },
+    [[], [], [], [], [], [], []]
+  )
+  .reduce((result, arr) => [...result, ...arr.sort(sorter)], []);
+
+console.log(`Part 2: ${getSum()}`);
