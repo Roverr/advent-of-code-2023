c499ee2557c27ced934cc5d518b1085415bb013f
Imre Racz <imre@riltech.co>
2023-12-08 08:42:03 +0100

    feat(8.5): part two done

--- day_8/index.js
+++ day_8/index.js
@@ -1,7 +1,27 @@
 const { map } = require("./data");
 
+const commonMultiple = (counter) => {
+    const gcd = (a, b) => b == 0 ? a : gcd(b, a % b);
+    let results = counter;
+    while (true) {
+        let partResults = [];
+        while (results.length > 0) {
+            if (results.length === 1) {
+                partResults.push(results[0]);
+                break;
+            }
+            const [a, b] = results.splice(0, 2);
+            partResults.push(a * b / gcd(a, b));
+        }
+        if (partResults.length === 1) {
+            return partResults[0];
+        }
+        results = partResults;
+    }
+};
+
 const main = () => {
-  let destination = 'AAA';
+  let destination = "AAA";
   let steps = 0;
   do {
     for (let letter of map.steps) {
@@ -15,6 +35,40 @@ const main = () => {
   } while (destination !== "ZZZ");
 
   console.log(`Part 1: ${steps}`);
+
+  const endsWith = {
+    A: [],
+    Z: [],
+  };
+  for (let key of map.directions.keys()) {
+    if (key[2] === "A") {
+      endsWith.A.push(key);
+    }
+    if (key[2] === "Z") {
+      endsWith.Z.push(key);
+    }
+  }
+
+  const cursors = JSON.parse(JSON.stringify(endsWith.A));
+  let counter = cursors.map(() => 0);
+  const areWeThereYet = () => counter.every((c) => c > 0);
+  steps = 0;
+  do {
+    for (let letter of map.steps) {
+      steps++;
+      for (let i = 0; i < cursors.length; i++) {
+        cursors[i] =
+          letter === "L"
+            ? map.directions.get(cursors[i]).left
+            : map.directions.get(cursors[i]).right;
+      }
+    }
+    counter = counter.map((v, i) =>
+      v > 0 ? v : endsWith.Z.includes(cursors[i]) ? steps : 0
+    );
+  } while (!areWeThereYet());
+
+  console.log(`Part 2: ${commonMultiple(counter.map(c => BigInt(c)))}`);
 };
 
 main();
